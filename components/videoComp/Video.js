77862c96f4918c5c9ed1416d1acd1acec59cfccd
sheraz <sherazzafar148@gmail.com>
2023-03-06 15:53:02 +0500

    ok

--- components/videoComp/Video.js
+++ components/videoComp/Video.js
@@ -7,26 +7,6 @@ import baseUrl from '../../utils/baseUrl'
 const Video = ({ src, sub, user }) => {
     const router = useRouter();
     const id = user._id
-<<<<<<< HEAD
-    const [showVideo, setShowVideo] = useState(false);
-
-    const handleClick = () => {
-      setShowVideo(!showVideo);
-    };
-const del = async ()=>{
-    
-    {
-        try{
-
-           const send= await axios.delete(`${baseUrl}/signup/delete/${id}`)
-           router.push('/home')
-          
-           
-            console.log(send)
-        }catch(e){
-            console.log(e)
-            router.push('/home')
-=======
     const del = async () => {
         {
             try {
@@ -39,7 +19,6 @@ const del = async ()=>{
             } catch (e) {
                 console.log(e)
             }
->>>>>>> b42627557a7bcbe432068d01aaa841f758ae969e
         }
     }
     React.useEffect(() => {
@@ -49,40 +28,6 @@ const del = async ()=>{
     console.log(sub);
     return (
         <>
-<<<<<<< HEAD
-        <video
-            controls
-            autoPlay
-            controlsList='nodownload'
-            className='myVideoPlayer'
-            onClick={handleClick}
-
-        >
-            <source src={src} type="video/mp4" />
-            <source src={src} type="video/webm" />
-            <track
-                label='English'
-                kind='subtitles'
-                srcLang='en'
-                src={sub}
-                default
-            />
-        </video>
-        
-        {/* <button onClick={handleButtonClick}>Click me</button> */}
-     
-        {showVideo && <button className='set' onClick={del}> 
-        
-        <div className={styles.crossicon} >
-        <div className={styles.crossline}></div>
-        <div className={styles.crossline}></div>
-
-        </div>
-        
-         </button>} 
-            
-          
-=======
             <video
                 controls
                 autoPlay
@@ -108,7 +53,6 @@ const del = async ()=>{
             </button>
 
 
->>>>>>> b42627557a7bcbe432068d01aaa841f758ae969e
         </>
     )
 }
